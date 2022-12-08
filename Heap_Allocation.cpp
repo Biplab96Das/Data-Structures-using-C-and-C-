de31fe92b3d54d852f17522b929120324dbaa765
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-08 15:44:22 +0530

    Add files via upload

---
+++ Heap_Allocation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+  int *p=(int *)malloc(sizeof(int)*5);//Heap Allaocation using pointer in C
+  p[0]=0;p[1]=-1;p[2]=-2;p[3]=3;p[4]=-4;
+  int *Point=new int[5];//Heap Allocation in C++ using pointer in C++
+  Point[0]=0;Point[1]=-1;Point[2]=-2;Point[3]=3;Point[3]=3;Point[4]=-4;
+  for(int i=0;i<5;++i)
+     cout<<p[i]<<" ";
+  cout<<"\n";
+  for(int i=0;i<5;++i)
+     cout<<Point[i]<<" ";
+  cout<<"\n";       
+  return 0;
+}
