1154ff27884bf197d7d9734fb15f5a33b206ab45
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-09 15:08:39 +0530

    Update Pointer2_Struct.cpp

--- Pointer2_Struct.cpp
+++ Pointer2_Struct.cpp
@@ -7,15 +7,20 @@ struct Rectangle
     int breadth;
 };
 int main()
-{
+{         //*Store structure in stack*
  Rectangle r={10,5};//Accessing Rectangle using normal variable
- cout<<"Area= "<<r.length*r.breadth<<endl;   
+ cout<<"Area= "<<r.length*r.breadth<<endl;
+ Rectangle *q=&r;
+ (*q).length=4;
+ q->breadth=6;
+ cout<<"Area= "<<(*q).breadth*q->length<<endl;
+         //*Store structure in Heap*   
  Rectangle *p=(struct Rectangle *)malloc(sizeof(struct Rectangle));//Unlike C,C++ doesn't
  //require to declare struct before Rectangle variable
  (*p).length=12;//Accessing through pointer and dot
  (*p).breadth=12;
  cout<<"Area= "<<(*p).length*(*p).breadth<<endl;
- p->length=10;//Accessing using dereference
+ p->length=10;//Accessing using dereference operator
  p->breadth=5;
  cout<<"Area= "<<p->length*p->breadth<<endl;
  return 0;  
